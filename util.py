f893e69ad4cff50d911e3e68a07837a762bffa3b
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2015-11-24 20:06:32 +0900

    Added CircularList to utils

--- util.py
+++ util.py
@@ -1,5 +1,6 @@
 
 from __future__ import print_function
+import numpy as np
 
 class Logger(object):
     def __init__(self, levels, print_threshold, path, prepend=None, formatter=None):
@@ -39,3 +40,69 @@ def product(*args):
     for arg in args:
         res *= arg
     return res
+
+
+class CircularList(object):
+    """A circular list that stores items in order.
+    It is intended for use in experience replay.
+
+    It support uniform random sampling, usual indexing, and slice notation.
+    """
+
+    def __init__(self, length):
+        self.data = [None for _ in xrange(length)]
+        self.i = -1 # Index of the last changed object
+        self.full_circle = False # Flag set to True when the whole list is filled out and we start looping
+
+    def insert(self, value):
+        self.i += 1
+        if self.i >= len(self.data):
+            self.i = 0
+            self.full_circle = True
+        self.data[self.i] = value
+
+    def batch_insert(self, values):
+        for v in values:
+            self.insert(v)
+
+    def uniform_random_sample(self, sample_size):
+        if self.full_circle:
+            data = self.data
+        else:
+            data = self.data[:self.i+1]
+        return np.random.choice(data, sample_size, replace=True) # uniform
+
+    def _is_valid_index(self, index):
+        return index >= 0 and ((index < len(self.data)) if self.full_circle else (index <= self.i))
+
+    def _get_single_item(self, index):
+        if self.i == -1:
+            raise IndexError("No elements in CircularList yet.")
+        if not self._is_valid_index(index):
+            raise IndexError("Invalid index {}. Should be in range (0, {})".format(index, (len(self.data) if self.full_circle else self.i) ))
+
+        i = self.i - index
+        if self.full_circle:
+            i = i % len(self.data)
+
+        return self.data[i]
+
+    def __getitem__(self, key):
+        """index 0 is the most recent inserted object.
+        index length-1 is the oldest object in memory.
+
+        Supports slice notation
+        """
+        if type(key) == slice:
+            start = key.start or 0
+            stop = key.stop or (len(self.data) if self.full_circle else self.i + 1)
+            step = key.step or 1
+
+            return [self[i] for i in xrange(start, stop, step)]
+        else:
+            return self._get_single_item(key)
+
+    def __len__(self):
+        if self.full_circle:
+            return len(self.data)
+        return self.i + 1
