e04dd9815e74dcd542f48e5c850103262ed81136
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2015-11-27 02:00:58 +0900

    Generalized run_experiment to avoid having to edit it

--- run_experiment.py
+++ run_experiment.py
@@ -1,34 +1,42 @@
 #!/usr/bin/env python
 
+import argparse
+import os
+
+
+EXPERIMENTS_PACKAGE = "experiments"
+
+
+def load_experiment(name):
+    name = EXPERIMENTS_PACKAGE + "." + name
+    return __import__(name, fromlist=[''])
+
 
 def main():
-    import argparse
-
     parser = argparse.ArgumentParser(
         description='Experiment handler for pyagents using ALE.')
-    parser.add_argument('experiment', 
+    parser.add_argument('experiment',
                         help=('The name of the experiment you want to run. '
                               'If it is NAME, there should be a file in '
                               'experiments/NAME.py that runs the actual '
-                              'experiment. Additionally, this file should be '
-                              'modified to contain the necessary information '
-                              'about that experiment.'))
+                              'experiment.')
+                        )
 
-    available = ("testbed",
-                 "testbed2")
+    available = os.listdir(EXPERIMENTS_PACKAGE)
+    
+    # only .py files (not __init__), and remove the .py
+    available = [d[:-3]
+                 for d in available if d.endswith('.py') and (not '__init__' in d)]
 
     experiment = parser.parse_args().experiment
 
-    if experiment == 'testbed':
-        from experiments import testbed
-    elif experiment == 'testbed2':
-        from experiments import testbed2
+    if experiment in available:
+        load_experiment(experiment)
     else:
         print "No experiment known under the name '{}'".format(experiment)
         print "Available experiments are"
         for v in sorted(available):
             print "\t{}".format(v)
-
 
 if __name__ == '__main__':
     main()
