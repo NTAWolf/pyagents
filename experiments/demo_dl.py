03fd02f912c1d0efea19bde880772de0509c6150
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2015-12-01 03:07:09 +0900

    Simplified no-graphics demo_dl

--- experiments/demo_dl.py
+++ experiments/demo_dl.py
@@ -3,5 +3,7 @@ from agents import DLAgent
 
 agent = DLAgent(10)
 gm = GameManager("space_invaders.bin", agent, 'results/testbed3',
-                 remove_old_results_dir=True, use_minimal_action_set=True, visualise='rgb')
+                 remove_old_results_dir=True, use_minimal_action_set=True, 
+                 visualise=None)
+                 # visualise='rgb')
 gm.run(n_episodes=5)
