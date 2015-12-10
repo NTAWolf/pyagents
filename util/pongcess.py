4e7bceacd978ba667c2a63d01117e95c537fa114
Thorbjørn Wolf <thorbjorn.wolf@gmail.com>
2015-12-11 04:22:48 +0900

    Implemented ball intercept prediction

--- util/pongcess.py
+++ util/pongcess.py
@@ -13,11 +13,11 @@ AGENT_X = 140 # to 143 (incl)
 OPPONENT_COLOR = 56
 OPPONENT_X = 19 # to 16 (incl)
 
-PLAYAREA_MIN = 34
-PLAYAREA_MAX = 194
+PLAY_AREA_TOP = 34
+PLAY_AREA_BOTTOM = 194
 
 X_RANGE = np.arange(160)
-Y_RANGE = np.arange(0, PLAYAREA_MAX - PLAYAREA_MIN)
+Y_RANGE = np.arange(0, PLAY_AREA_BOTTOM - PLAY_AREA_TOP)
 
 class Feature(object):
 
@@ -45,6 +45,81 @@ class StateIndex(Feature):
         return list(self.state2index.iterkeys())
 
 
+class BallIntercept(Feature):
+    """Based on the ball's position last time process was called,
+    this class predicts where the ball will intercept with the 
+    agent's paddle space.
+    """
+    def __init__(self, raw_state_callbacks):
+        self.pos = Positions(raw_state_callbacks)
+        self.play_area_size = np.array([AGENT_X - OPPONENT_X,
+                                        PLAY_AREA_BOTTOM - PLAY_AREA_TOP],
+                                        dtype=np.float32)
+        self.last_ball = self.play_area_size / 2
+        self.LEFT = 0
+        self.TOP = 1
+        self.RIGHT = 2
+        self.BOTTOM = 3
+
+    def process(self):
+        """Returns an integer
+        """
+        self.pos.update()
+        ball = np.array(self.pos.ball, dtype=np.float32)
+
+        vel = ball - self.last_ball
+        self.last_ball = ball.copy()
+        
+        edge = -1
+        while edge != self.RIGHT:
+            ball, vel, edge = self.next_intercept(ball, vel)
+
+        return int(ball[1])
+
+    def next_intercept(self, pos, vel):
+        """Returns edge intercept point and reflected velocity
+        around edge normal, as well as intercept edge constant
+        """
+        reflected_vel = vel * (-1, 1)
+        # Moving left?
+        if vel[0] < 0:
+            # a is the multiplier on vel to achieve left side intercept
+            a = (OPPONENT_X - pos[0])/vel[0]
+            intercept = pos + a*vel
+            # Do we hit the left edge?
+            if PLAY_AREA_TOP <= intercept[1] <= PLAY_AREA_BOTTOM:
+                return intercept, reflected_vel, self.LEFT
+            return self.top_bottom_intercept(pos, vel)
+
+        # Otherwise, moving right
+        a = (AGENT_X - pos[0])/vel[0]
+        intercept = pos + a*vel
+        # Do we hit the right edge?
+        if PLAY_AREA_TOP <= intercept[1] <= PLAY_AREA_BOTTOM:
+            return intercept, reflected_vel, self.RIGHT
+        return self.top_bottom_intercept(pos, vel)
+
+    def top_bottom_intercept(self, pos, vel):
+        """Returns the next intercept with a top layer.
+        Only used when you most definitely know you'll not
+        hit a side edge.
+
+        Returns edge intercept point, reflected velocity
+        around edge normal, and edge constant
+        """
+        reflected_vel = vel * (1, -1)
+        # Moving down? (y-axis is inverted)
+        if vel[1] > 0:
+            # a is the multiplier on vel to achieve bottom edge intercept
+            a = (PLAY_AREA_BOTTOM - pos[1])/vel[1]
+            intercept = pos + a*vel
+            return intercept, reflected_vel, self.BOTTOM
+        # a is the multiplier on vel to achieve top edge intercept
+        a = (PLAY_AREA_TOP - pos[1])/vel[1]
+        intercept = pos + a*vel
+        return intercept, reflected_vel, self.TOP
+
+
 class RelativeBall(Feature):
     def __init__(self, raw_state_callbacks):
         self.pos = Positions(raw_state_callbacks)
@@ -70,7 +145,7 @@ class Positions(object):
         frame is a numpy array with the raw colours from a frame
         """
         frame = self.f().reshape((210,160))
-        frame = frame[PLAYAREA_MIN:PLAYAREA_MAX]
+        frame = frame[PLAY_AREA_TOP:PLAY_AREA_BOTTOM]
         self._update_agent(frame)
         self._update_opponent(frame)
         self._update_ball(frame)
